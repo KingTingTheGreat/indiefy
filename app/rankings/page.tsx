b93ddfdefb9bf810b14ec3277cd38fe36e39cfd4
Jeffrey <jting@bu.edu>
2024-01-26 10:34:44 -0500

    pages are server-side renderd. therefore can use db connection directly and do not need api. this is the beginning of testing

--- app/rankings/page.tsx
+++ app/rankings/page.tsx
@@ -1,6 +1,7 @@
 import { Leaderboard } from "@/components/leaderboard";
+import { usersDBConnect } from "@/utils/connection";
 
-const root = process.env.ROOT!;
+// const root = process.env.ROOT!;
 
 const processUsers = (users: any) => {
 	return users.map((user: any) => {
@@ -13,11 +14,15 @@ const processUsers = (users: any) => {
 
 export default async function Rankings() {
 	try {
-		const users = await fetch(root + "/api/users", {
-			headers: { authorization: process.env.API_AUTH! },
-		})
-			.then((response) => response.json())
-			.then((data) => processUsers(data));
+		// const users = await fetch(root + "/api/users", {
+		// 	headers: { authorization: process.env.API_AUTH! },
+		// })
+		// 	.then((response) => response.json())
+		// 	.then((data) => processUsers(data));
+		const db = await usersDBConnect();
+		const allUsers = await db.Users.find({});
+
+		const users = processUsers(allUsers);
 
 		return <Leaderboard users={users} />;
 	} catch (error) {
