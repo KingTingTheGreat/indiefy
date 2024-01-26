399830af4f3a6ab1e0c5a2c67da24a2cc62f3fba
KingTingTheGreat <jting@bu.edu>
2024-01-26 11:01:45 -0500

    distribution also uses database directly

--- app/rankings/page.tsx
+++ app/rankings/page.tsx
@@ -1,9 +1,6 @@
 import { Leaderboard } from "@/components/leaderboard";
 import { DBUser } from "@/types";
 import { usersDBConnect } from "@/utils/connection";
-import _default from "@emotion/react/_isolated-hnrs";
-
-// const root = process.env.ROOT!;
 
 const processUsers = (users: { _doc: DBUser }[]) => {
 	return users.map((user) => {
@@ -16,11 +13,6 @@ const processUsers = (users: { _doc: DBUser }[]) => {
 
 export default async function Rankings() {
 	try {
-		// const users = await fetch(root + "/api/users", {
-		// 	headers: { authorization: process.env.API_AUTH! },
-		// })
-		// 	.then((response) => response.json())
-		// 	.then((data) => processUsers(data));
 		const db = await usersDBConnect();
 		const allUsers = await db.Users.find({});
 
