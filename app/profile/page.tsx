ad4a82409028090ddef14169cf8389afd0d2657f
Jeffrey Ting <jting@bu.edu>
2024-05-24 22:26:07 -0500

    useSearchParams suspense build fix

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,44 +1,11 @@
-"use client";
-import { useEffect, useState } from "react";
-import TopSongs from "@/components/top-songs";
-import ScoreBar from "@/components/score-bar";
-import { redirect, useSearchParams } from "next/navigation";
-import { UserInfoResponse, Song } from "@/types";
+import { Suspense } from "react";
+import ProfileContent from "@/components/profile-content";
 
 const Profile = () => {
-	const searchParams = useSearchParams();
-
-	const [songs, setSongs] = useState<Song[]>([]);
-	const [score, setScore] = useState<number>(0);
-	const [token, setToken] = useState<string | null>(
-		searchParams.get("code") ?? localStorage.getItem("indiefy-token")
-	);
-
-	useEffect(() => {
-		if (token && token !== "undefined") {
-			fetch(`/api/user-info?token=${token}`)
-				.then((res) => res.json())
-				.then((data: UserInfoResponse) => {
-					// setToken(data.token);
-					// localStorage.setItem("indiefy-token", data.token);
-					console.log("logging fetched data");
-					console.log(data);
-					setSongs(data.songs);
-					setScore(data.score);
-				});
-			localStorage.setItem("indiefy-token", token);
-		} else {
-			console.log("no token");
-			redirect("/");
-		}
-	}, []);
-
 	return (
-		<div className="flex flex-col items-center">
-			<p>your profile</p>
-			<ScoreBar score={score} />
-			<TopSongs songs={songs} />
-		</div>
+		<Suspense fallback={<div>Loading...</div>}>
+			<ProfileContent />
+		</Suspense>
 	);
 };
 
