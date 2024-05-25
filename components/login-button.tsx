9f299e1ab0511fa09a431f55eb94d65436b4e4c0
Jeffrey Ting <jting@bu.edu>
2024-05-25 10:02:35 -0500

    redirect to temporary page for handshake with server

--- components/login-button.tsx
+++ components/login-button.tsx
@@ -5,7 +5,7 @@ const LoginButton = () => {
 	return (
 		<Link
 			className="m-4 px-5 py-2 w-fit text-2xl rounded-2xl bg-customGreen"
-			href={`https://accounts.spotify.com/authorize?response_type=code&client_id=1b1f629c114e4caba95204375a1db469&redirect_uri=https://indiefy.org/callback&scope=user-top-read`}>
+			href={`https://accounts.spotify.com/authorize?response_type=code&client_id=1b1f629c114e4caba95204375a1db469&redirect_uri=http://localhost:3000/callback&scope=user-top-read`}>
 			Login with Spotify
 		</Link>
 	);
