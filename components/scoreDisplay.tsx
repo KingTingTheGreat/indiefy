7ffbf9408ae26417695426a4406feeb8227f9dfb
Jeffrey Ting <jeffrey_ting@cargill.com>
2024-08-07 13:28:52 -0500

    styling

--- components/scoreDisplay.tsx
+++ components/scoreDisplay.tsx
@@ -1,6 +1,6 @@
 const ScoreDisplay = ({ score }: { score: number | undefined }) =>
   score ? (
-    <div className="flex flex-row justify-start items center w-[40%] bg-nullGray m-4">
+    <div className="flex flex-row justify-start items center w-[40%] max-sm:w-[80%] bg-nullGray m-4">
       <div className="bg-customGreen h-[90%" style={{ width: `${score}%` }}>
         <p>{score}%</p>
       </div>
